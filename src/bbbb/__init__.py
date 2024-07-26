27794dffcbe8a195db8391036e3428341b01b019
Karl Knechtel <zahlman@proton.me>
2024-07-26 18:41:14 -0400

    Add basic metadata to wheel
    A proper `.dist-info` folder is created within the wheel, and populated
    with METADATA (recreated from scratch - copying PKG-INFO would be about as
    much work, and less robust) and WHEEL files. The LICENSE is also copied
    from the project root (it's assumed to be present and have no
    extension).

--- src/bbbb/__init__.py
+++ src/bbbb/__init__.py
@@ -13,7 +13,10 @@ PLATFORM_TAG = 'any'
 # According to the specifications on packaging.python.org, for a
 # pyproject.toml-based sdist, we must conform to version 2.2 or later.
 # So we will specify version 2.2 even though we are only using basic features.
-METADATA = f"Metadata-Version: 2.2\nName: {NAME}\nVersion: {VERSION}"
+METADATA = f'Metadata-Version: 2.2\nName: {NAME}\nVersion: {VERSION}'
+WHEEL = f'Wheel-Version: 1.0\nGenerator: bbbb 0.1.0\nRoot-Is-Purelib: true\nTag: {PYTHON_TAG}-{ABI_TAG}-{PLATFORM_TAG}'
+# Also need a RECORD file and to copy LICENSE into the .dist-info.
+# Also should support making entry_points.txt.
 
 
 def _exclude_hidden_and_special_files(archive_entry):
@@ -49,7 +52,7 @@ def build_wheel(
     # these arguments positionally and in this specific order.
     wheel_directory, config_settings=None, metadata_directory=None
 ):
-    wheel_name = f"{NAME}-{VERSION}-{PYTHON_TAG}-{ABI_TAG}-{PLATFORM_TAG}.whl"
+    wheel_name = f'{NAME}-{VERSION}-{PYTHON_TAG}-{ABI_TAG}-{PLATFORM_TAG}.whl'
     wheel_path = Path(wheel_directory) / wheel_name
     # Assume src layout and that metadata has already been created therein.
     to_include = Path('src')
@@ -62,4 +65,10 @@ def build_wheel(
             for file in files:
                 path /= file
                 wheel.write(path, arcname=path.relative_to(to_include))
+        # Generate the .dist-info folder.
+        di = Path(f'{NAME}-{VERSION}.dist-info')
+        wheel.writestr(str(di / 'METADATA'), METADATA)
+        wheel.writestr(str(di / 'WHEEL'), WHEEL)
+        # TODO: RECORD, entry_points.txt
+        wheel.write('LICENSE', arcname=di / 'LICENSE')
     return wheel_name
