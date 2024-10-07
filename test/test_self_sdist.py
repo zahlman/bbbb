aef89e7f9a7eb4ded0c5b727f4e07563089ceda8
Karl Knechtel <zahlman@proton.me>
2024-10-06 21:16:43 -0400

    Use `build` frontend in testing
    This way we don't need to work through the PEP 517 API directly and get
    the subprocess hooks etc. for free. Notably, this means we don't need to
    use a `sys.path` hack to get at the current `bbbb` source code - in
    fact, we use the code from the copied pytest environment (thanks to the
    `backend-path` specification in `pyproject.toml`).
    
    Also, notably, this emulates the *recommended* workflow for actual use.
    
    The programmatic API for `build` doesn't directly expose the "build
    wheel from sdist" logic, so we'll still need a test flow that unpacks
    the sdist within the test environment and then builds a wheel from it.
    But since this is happening within a temporary folder already created
    by Pytest, we don't need to do any cleanup.

--- test/test_self_sdist.py
+++ test/test_self_sdist.py
@@ -1,3 +1,4 @@
+import build
 import os
 from pathlib import Path
 import pytest
@@ -6,14 +7,6 @@ from tarfile import open as open_tar
 
 
 project = Path(__file__).parent.parent
-# Since the project doesn't handle editable wheels yet, it can't install
-# itself in editable mode for testing. To ensure we test the in-progress
-# code (without having to uninstall/build/reinstall manually), we hack
-# sys.path.
-import sys
-sys.path.insert(0, str(project / 'src'))
-print(sys.path)
-import bbbb
 
 
 @pytest.fixture
@@ -42,6 +35,6 @@ def _verify_sdist(root_name, manifest_file):
 def test_self_sdist(copy_self):
     tmpdir = copy_self()
     os.chdir(tmpdir)
-    bbbb.build_sdist('test_dist')
+    build.ProjectBuilder('.').build('sdist', 'test_dist')
     assert 'test_dist' in os.listdir()
     _verify_sdist('bbbb-0.3.0', project / 'test' / 'self_manifest.txt')
