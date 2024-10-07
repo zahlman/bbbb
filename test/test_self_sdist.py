44224b9ca1f93984a7767dbdce72a69c4c6d7781
Karl Knechtel <zahlman@proton.me>
2024-10-06 20:07:31 -0400

    Test for contents of self-built sdist
    For now, the expected file name is hard-coded and the names of files in the
    produced archive are compared to a hard-coded "manifest".

--- test/test_self_sdist.py
+++ test/test_self_sdist.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 import pytest
 from shutil import copytree
+from tarfile import open as open_tar
 
 
 project = Path(__file__).parent.parent
@@ -23,9 +24,18 @@ def copy_self(tmpdir):
     return copy
 
 
+def _sdist_contents(archive, root):
+    with open_tar(archive) as t:
+        return sorted(m.path.replace(root, '.') for m in t.getmembers())
+
+
 def test_self_sdist(copy_self):
     tmpdir = copy_self()
     os.chdir(tmpdir)
     bbbb.build_sdist('test_sdist')
     assert 'test_sdist' in os.listdir()
-    
+    assert _sdist_contents('test_sdist/bbbb-0.3.0.tar.gz', 'bbbb-0.3.0') == [
+        '.',
+        './LICENSE.txt', './PKG-INFO', './README.md', './pyproject.toml',
+        './src', './src/bbbb', './src/bbbb/__init__.py', './src/bbbb/wheel.py'
+    ]
