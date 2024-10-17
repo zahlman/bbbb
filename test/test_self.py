d9c5de4405a4e80917fcea38ffcc331faebf7b31
Karl Knechtel <zahlman@proton.me>
2024-10-17 08:33:19 -0400

    New test configuration
    A single TOML file is used to store information for testing: the
    expected manifest for both sdist and wheel results, and hashes and file
    sizes from RECORD. Placeholders for the project (not package!) name and
    version can be substituted in, using vanilla `str.format`.
    
    The file format also specially indicates the path to the RECORD file in
    the wheel - this may not be necessary; TBD.
    
    The expected name and version should be read from the original
    `pyproject.toml` from the test data. This is less maintenance overall;
    in particular, it will mean the self-test data won't need to be updated
    simply because the project version number was changed.
    
    The tests have been updated to parse and use this file and thus verify
    the sdist and wheel file manifests.
    
    Future tests will also verify:
    
    * The sdist's `pyproject.toml` specifies a matching name and version
    * The wheel's `RECORD` specifies the correct hashes and file sizes (this
      also implicitly verifies file contents) and has the expected
      self-referential line
    
    (Eventually the hash/file size data will be removed from the self-test
    so that it doesn't have to be updated every time the project code
    changes)

--- test/test_self.py
+++ test/test_self.py
@@ -4,9 +4,14 @@ from pathlib import Path
 from shutil import copytree
 from tarfile import open as open_tar
 from zipfile import ZipFile
+
 # third-party
 import build, pytest
 
+try:
+    from tomllib import load as load_toml # requires 3.11+
+except ImportError: # 3.10 or earlier, so not in the standard library
+    from tomli import load as load_toml # third-party implementation.
 
 project = Path(__file__).parent.parent
 
@@ -39,18 +44,31 @@ def _read_manifest(name):
     return sorted(e for e in entries if e)
 
 
+def _read_config(filename, project_name, project_version):
+    with open(filename, 'rb') as f:
+        result = load_toml(f) # let errors propagate
+    # Do template substitution and sorting.
+    s = lambda t: t.format(name=project_name, version=project_version)
+    result['sdist']['files'] = sorted(map(s, result['sdist']['files']))
+    result['wheel']['files'] = sorted(map(s, result['wheel']['files']))
+    result['wheel']['record'] = s(result['wheel']['record'])
+    result['wheel']['sha256'] = sorted(map(s, result['wheel']['sha256']))
+    result['wheel']['size'] = sorted(map(s, result['wheel']['size']))
+    return result
+
+
 def test_self_sdist(copy_self):
     build.ProjectBuilder('.').build('sdist', 'test_dist')
-    expected = _read_manifest(project / 'test' / 'self_sdist_manifest.txt')
+    expected = _read_config(project / 'test' / 'expected.toml', 'bbbb', '0.3.0')
     actual = _list_tar_contents(f'test_dist/bbbb-0.3.0.tar.gz')
-    assert expected == actual
+    assert expected['sdist']['files'] == actual
 
 
 def test_self_wheel(copy_self):
     build.ProjectBuilder('.').build('wheel', 'test_dist')
-    expected = _read_manifest(project / 'test' / 'self_wheel_manifest.txt')
+    expected = _read_config(project / 'test' / 'expected.toml', 'bbbb', '0.3.0')
     actual = _list_zip_contents('test_dist/bbbb-0.3.0-py3-none-any.whl')
-    assert expected == actual
+    assert expected['wheel']['files'] == actual
 
 
 def test_self_wheel_via_sdist(copy_self):
@@ -59,6 +77,6 @@ def test_self_wheel_via_sdist(copy_self):
     with open_tar(f'test_dist/{name}.tar.gz') as t:
         t.extractall(f'test_dist')
     build.ProjectBuilder(f'test_dist/{name}').build('wheel', 'test_dist')
-    expected = _read_manifest(project / 'test' / 'self_wheel_manifest.txt')
+    expected = _read_config(project / 'test' / 'expected.toml', 'bbbb', '0.3.0')
     actual = _list_zip_contents('test_dist/bbbb-0.3.0-py3-none-any.whl')
-    assert expected == actual
+    assert expected['wheel']['files'] == actual
