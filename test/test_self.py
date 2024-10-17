f9f0e66b4f91b017dbab3190745dd6e96cb12fff
Karl Knechtel <zahlman@proton.me>
2024-10-17 10:06:20 -0400

    Refactoring
    The test fixture is now a callable again (so that the paths for the test
    project source and TOML config can be specified), and it handles reading
    the config as well as `pyproject.toml`.

--- test/test_self.py
+++ test/test_self.py
@@ -22,14 +22,32 @@ def _toplevel_dotfiles(src, names):
     return [n for n in names if n.startswith('.')] if is_root else []
 
 
+def _read_config(filename, project_name, project_version):
+    with open(filename, 'rb') as f:
+        result = load_toml(f) # let errors propagate
+    # Do template substitution and sorting.
+    s = lambda t: t.format(name=project_name, version=project_version)
+    result['sdist']['files'] = sorted(map(s, result['sdist']['files']))
+    result['wheel']['files'] = sorted(map(s, result['wheel']['files']))
+    result['wheel']['record'] = s(result['wheel']['record'])
+    result['wheel']['sha256'] = sorted(map(s, result['wheel']['sha256']))
+    result['wheel']['size'] = sorted(map(s, result['wheel']['size']))
+    return result
+
+
 @pytest.fixture
 def setup(tmpdir):
-    copytree(BBBB_ROOT, tmpdir, ignore=_toplevel_dotfiles, dirs_exist_ok=True)
-    os.chdir(tmpdir)
-    # Determine project name and version for use in tests.
-    with open('pyproject.toml', 'rb') as f:
-        project = load_toml(f)['project']
-    return project['name'], project['version']
+    def _copy_project_and_get_metadata(project_path, config_rel_path):
+        copytree(project_path, tmpdir, ignore=_toplevel_dotfiles, dirs_exist_ok=True)
+        os.chdir(tmpdir)
+        # Determine project name and version for use in tests.
+        with open('pyproject.toml', 'rb') as f:
+            project = load_toml(f)['project']
+        name, version = project['name'], project['version']
+        # Determine expectations for resulting sdist and wheel.
+        toml_path = BBBB_ROOT / 'test' / config_rel_path / 'expected.toml'
+        return _read_config(toml_path, name, version), name, version
+    return _copy_project_and_get_metadata
 
 
 def _list_tar_contents(name):
@@ -48,52 +66,35 @@ def _read_manifest(name):
     return sorted(e for e in entries if e)
 
 
-def _read_config(filename, project_name, project_version):
-    with open(filename, 'rb') as f:
-        result = load_toml(f) # let errors propagate
-    # Do template substitution and sorting.
-    s = lambda t: t.format(name=project_name, version=project_version)
-    result['sdist']['files'] = sorted(map(s, result['sdist']['files']))
-    result['wheel']['files'] = sorted(map(s, result['wheel']['files']))
-    result['wheel']['record'] = s(result['wheel']['record'])
-    result['wheel']['sha256'] = sorted(map(s, result['wheel']['sha256']))
-    result['wheel']['size'] = sorted(map(s, result['wheel']['size']))
-    return result
-
-
 def _build(kind, src):
     build.ProjectBuilder(src).build(kind, 'test_dist')
 
 
-def _verify_sdist(src_path, config_rel_path, name, version):
+def _verify_sdist(src_path, config_rel_path, expected, name, version):
     _build('sdist', src_path)
-    toml_path = BBBB_ROOT / 'test' / config_rel_path / 'expected.toml'
-    expected = _read_config(toml_path, name, version)
     actual = _list_tar_contents(f'test_dist/{name}-{version}.tar.gz')
     assert expected['sdist']['files'] == actual
 
 
-def _verify_wheel(src_path, config_rel_path, name, version):
+def _verify_wheel(src_path, config_rel_path, expected, name, version):
     _build('wheel', src_path)
-    toml_path = BBBB_ROOT / 'test' / config_rel_path / 'expected.toml'
-    expected = _read_config(toml_path, name, version)
     actual = _list_zip_contents(f'test_dist/{name}-{version}-py3-none-any.whl')
     assert expected['wheel']['files'] == actual
 
 
 def test_self_sdist(setup):
-    name, version = setup
-    _verify_sdist('.', '.', name, version)
+    expected, name, version = setup(BBBB_ROOT, '.')
+    _verify_sdist('.', '.', expected, name, version)
 
 
 def test_self_wheel(setup):
-    name, version = setup
-    _verify_wheel('.', '.', name, version)
+    expected, name, version = setup(BBBB_ROOT, '.')
+    _verify_wheel('.', '.', expected, name, version)
 
 
 def test_self_wheel_via_sdist(setup):
-    name, version = setup
+    expected, name, version = setup(BBBB_ROOT, '.')
     _build('sdist', '.')
     with open_tar(f'test_dist/{name}-{version}.tar.gz') as t:
         t.extractall(f'test_dist')
-    _verify_wheel(f'test_dist/{name}-{version}', '.', name, version)
+    _verify_wheel(f'test_dist/{name}-{version}', '.', expected, name, version)
