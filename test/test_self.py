f7989116f8a290759c81dfbdb96a646409a28b05
Karl Knechtel <zahlman@proton.me>
2024-10-17 10:27:21 -0400

    Cleanup

--- test/test_self.py
+++ test/test_self.py
@@ -8,10 +8,12 @@ from zipfile import ZipFile
 # third-party
 import build, pytest
 
+
 try:
     from tomllib import load as load_toml # requires 3.11+
 except ImportError: # 3.10 or earlier, so not in the standard library
     from tomli import load as load_toml # third-party implementation.
+
 
 BBBB_ROOT = Path(__file__).parent.parent
 
@@ -54,35 +56,21 @@ def setup(tmpdir):
     return _impl
 
 
-def _list_tar_contents(name):
-    with open_tar(name) as t:
-        return sorted(m.path for m in t.getmembers())
-
-
-def _list_zip_contents(name):
-    with ZipFile(name) as z:
-        return sorted(m.orig_filename for m in z.filelist)
-
-
-def _read_manifest(name):
-    with open(name) as f:
-        entries = ['' if line.startswith('#') else line.rstrip() for line in f]
-    return sorted(e for e in entries if e)
-
-
 def _build(kind, src):
     build.ProjectBuilder(src).build(kind, 'test_dist')
 
 
 def _verify_sdist(src_path, config_rel_path, expected, name, version):
     _build('sdist', src_path)
-    actual = _list_tar_contents(f'test_dist/{name}-{version}.tar.gz')
+    with open_tar(f'test_dist/{name}-{version}.tar.gz') as t:
+        actual = sorted(m.path for m in t.getmembers())
     assert expected['sdist']['files'] == actual
 
 
 def _verify_wheel(src_path, config_rel_path, expected, name, version):
     _build('wheel', src_path)
-    actual = _list_zip_contents(f'test_dist/{name}-{version}-py3-none-any.whl')
+    with ZipFile(f'test_dist/{name}-{version}-py3-none-any.whl') as z:
+        actual = sorted(m.orig_filename for m in z.filelist)
     assert expected['wheel']['files'] == actual
 
 
