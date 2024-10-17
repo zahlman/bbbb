dde29a948e7571a4ad60e3716c6787f57da24fff
Karl Knechtel <zahlman@proton.me>
2024-10-17 08:56:28 -0400

    Refactoring

--- test/test_self.py
+++ test/test_self.py
@@ -13,19 +13,23 @@ try:
 except ImportError: # 3.10 or earlier, so not in the standard library
     from tomli import load as load_toml # third-party implementation.
 
-project = Path(__file__).parent.parent
+BBBB_ROOT = Path(__file__).parent.parent
 
 
 def _toplevel_dotfiles(src, names):
     # a filter for copytree
-    is_root = (Path(src) == project)
+    is_root = (Path(src) == BBBB_ROOT)
     return [n for n in names if n.startswith('.')] if is_root else []
 
 
 @pytest.fixture
-def copy_self(tmpdir):
-    copytree(project, tmpdir, ignore=_toplevel_dotfiles, dirs_exist_ok=True)
+def setup(tmpdir):
+    copytree(BBBB_ROOT, tmpdir, ignore=_toplevel_dotfiles, dirs_exist_ok=True)
     os.chdir(tmpdir)
+    # Determine project name and version for use in tests.
+    with open('pyproject.toml', 'rb') as f:
+        project = load_toml(f)['project']
+    return project['name'], project['version']
 
 
 def _list_tar_contents(name):
@@ -57,26 +61,32 @@ def _read_config(filename, project_name, project_version):
     return result
 
 
-def test_self_sdist(copy_self):
-    build.ProjectBuilder('.').build('sdist', 'test_dist')
-    expected = _read_config(project / 'test' / 'expected.toml', 'bbbb', '0.3.0')
-    actual = _list_tar_contents(f'test_dist/bbbb-0.3.0.tar.gz')
+def _build(kind, *, src='.', dst='test_dist'):
+    build.ProjectBuilder(src).build(kind, dst)
+
+
+def test_self_sdist(setup):
+    name, version = setup
+    _build('sdist')
+    expected = _read_config(BBBB_ROOT / 'test' / 'expected.toml', name, version)
+    actual = _list_tar_contents(f'test_dist/{name}-{version}.tar.gz')
     assert expected['sdist']['files'] == actual
 
 
-def test_self_wheel(copy_self):
-    build.ProjectBuilder('.').build('wheel', 'test_dist')
-    expected = _read_config(project / 'test' / 'expected.toml', 'bbbb', '0.3.0')
-    actual = _list_zip_contents('test_dist/bbbb-0.3.0-py3-none-any.whl')
+def test_self_wheel(setup):
+    name, version = setup
+    _build('wheel')
+    expected = _read_config(BBBB_ROOT / 'test' / 'expected.toml', name, version)
+    actual = _list_zip_contents(f'test_dist/{name}-{version}-py3-none-any.whl')
     assert expected['wheel']['files'] == actual
 
 
-def test_self_wheel_via_sdist(copy_self):
-    build.ProjectBuilder('.').build('sdist', 'test_dist')
-    name = 'bbbb-0.3.0'
-    with open_tar(f'test_dist/{name}.tar.gz') as t:
+def test_self_wheel_via_sdist(setup):
+    name, version = setup
+    _build('sdist')
+    with open_tar(f'test_dist/{name}-{version}.tar.gz') as t:
         t.extractall(f'test_dist')
-    build.ProjectBuilder(f'test_dist/{name}').build('wheel', 'test_dist')
-    expected = _read_config(project / 'test' / 'expected.toml', 'bbbb', '0.3.0')
-    actual = _list_zip_contents('test_dist/bbbb-0.3.0-py3-none-any.whl')
+    _build('wheel', src=f'test_dist/{name}-{version}')
+    expected = _read_config(BBBB_ROOT / 'test' / 'expected.toml', name, version)
+    actual = _list_zip_contents(f'test_dist/{name}-{version}-py3-none-any.whl')
     assert expected['wheel']['files'] == actual
