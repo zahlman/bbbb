07f3021066228fcd62532bad3a4b4b66053b6b10
Karl Knechtel <zahlman@proton.me>
2024-10-18 14:28:34 -0400

    Add option to exclude tests
    Multiple fixes for the sdist filter:
    
    * It looks for tests in the right place.
    
    * It checks command-line config and uses a separate function to do the
      main work after verifying that config. For sanity, any unrecognized
      or ill-formed command-line options are rejected.
    
    * When the `build` command line specifies `-C exclude-tests` (which is
      equivalent to passing `{'exclude-tests': ''}` in the API), the filter
      will exclude tests; otherwise the tests are included.
    
    TODO: make it so that the filter is only invoked once with a generator
    of file paths or something.
    
    Currently, wheel tests fail because the direct-wheel path can't actually
    exclude tests, and the wheel-via-sdist path is set up to include them
    (because it's supposed to verify that the wheel can filter things that
    the sdist included).

--- sdist.py
+++ sdist.py
@@ -1,4 +1,4 @@
-def example_filter(config, path):
+def _filter_implementation(path, exclude_tests):
     if path.name.startswith('.'):
         return False
     parts = path.parts
@@ -9,7 +9,18 @@ def example_filter(config, path):
     # sdists and wheels created either normally or in tests.
     if parts[0] in ('dist', 'test_dist'):
         return False
-    # Exclude src/tests for now. TODO make this optional
-    if parts[:2] == ('src', 'tests'):
+    if 'tests' in parts and exclude_tests:
         return False
     return True
+
+
+def example_filter(config, path):
+    exclude_tests = False
+    if 'exclude-tests' in config['commandline']:
+        if config['commandline']['exclude-tests'] != '':
+            raise ValueError("`exclude-tests` shouldn't have a value")
+        exclude_tests = True
+    bad_keys = set(config['commandline'].keys()) - {'exclude-tests'}
+    if bad_keys:
+        raise ValueError(f"unrecognized command-line options: {bad_keys}")
+    return _filter_implementation(path, exclude_tests)
